d2dd4e57dbb110eb0b75481238dd5c30e0e570f0
YE <yaakovde123@gmail.com>
2026-02-05 10:46:25 +0200

    created func to adjust func

--- add_to_budget.py
+++ add_to_budget.py
@@ -1,8 +1,13 @@
-def income_ticket_fair(money,budget_file):
+def income_outgoing_budget(money,budget_file,func):
+    money = int(money)
     with open(budget_file,"r") as f:
-        budget_before = int(f.read())
-    budget_after = budget_before+money
+        budget_before = f.read()
+        budget_before = int(budget_before)
+        if func == "+":
+            budget_after = money + budget_before
+        else:
+            budget_after = money - budget_before
+        budget_after = str(budget_after)
     with open(budget_file,"w") as f:
         f.write(budget_after)
     return
-    
