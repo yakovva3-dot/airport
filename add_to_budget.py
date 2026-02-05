94e3060136b3d90f3173cc153e72d60154c0dccb
YE <yaakovde123@gmail.com>
2026-02-05 12:09:23 +0200

    updated func to prevent overdraft

--- add_to_budget.py
+++ add_to_budget.py
@@ -7,7 +7,9 @@ def income_outgoing_budget(money,budget_file,func):
             budget_after = money + budget_before
         else:
             budget_after = money - budget_before
-        budget_after = str(budget_after)
-    with open(budget_file,"w") as f:
-        f.write(budget_after)
+            if budget_after > 0:
+                budget_after = str(budget_after)
+                with open(budget_file,"w") as f:
+                    f.write(budget_after)
+            return "cannot purchase since not enough money in budget"
     return
